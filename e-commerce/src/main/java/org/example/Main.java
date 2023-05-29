78d59bf54c790828f521a7c20f3845a0d21a4f15
SukmaNigraha <sukmanigraha87@gmail.com>
2023-05-30 01:24:53 +0800

    membuat kelas data,server,request dan lain lain

--- e-commerce/src/main/java/org/example/Main.java
+++ e-commerce/src/main/java/org/example/Main.java
@@ -1,48 +1,21 @@
 package org.example;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
-import java.io.IOException;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
 import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8119), 0);
-
-        httpServer.createContext("/user", new userHandler()); //inisialisasi menggunakan UserHandler
-
-        httpServer.setExecutor(Executors.newSingleThreadExecutor());
-        httpServer.start();
-        System.out.println("connect");
-
-    }
-
-    public static class userHandler implements HttpHandler{
-
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            // POST -> create
-            // GET -> read
-            // PUT -> update
-            // DELETE -> delete
-            // CRUD
-
-            if ("GET".equals(exchange.getRequestMethod())) {
-                // send something intresting to user
-                OutputStream outputStream = exchange.getResponseBody();
-                String responseToBeSendBack = SQL.Connection();
-                exchange.sendResponseHeaders(200, responseToBeSendBack.length());
-
-
-                outputStream.write(responseToBeSendBack.getBytes());
-                outputStream.flush();
-                outputStream.close();
-            }
+        try{
+            HttpServer httpserver = HttpServer.create(new InetSocketAddress("localhost", 8119),0);
+            httpserver.createContext("/", new Server());
+//            test
+            httpserver.setExecutor(Executors.newSingleThreadExecutor());
+            httpserver.start();
+            System.out.println( "Listening on port 8119...");
+        }catch(Exception e){
+            e.printStackTrace();
         }
-
     }
 }
