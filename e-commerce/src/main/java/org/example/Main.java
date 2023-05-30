7bf6be525b23d19b18a2287b2b3a5b8a2ebd226a
SukmaNigraha <sukmanigraha87@gmail.com>
2023-05-30 23:00:00 +0800

    add final code

--- e-commerce/src/main/java/org/example/Main.java
+++ e-commerce/src/main/java/org/example/Main.java
@@ -1,20 +1,13 @@
 package org.example;
 
-import java.net.InetSocketAddress;
-import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
-import java.util.concurrent.Executors;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        try{
-            HttpServer httpserver = HttpServer.create(new InetSocketAddress("localhost", 8119),0);
-            httpserver.createContext("/", new Server());
-//            test
-            httpserver.setExecutor(Executors.newSingleThreadExecutor());
-            httpserver.start();
-            System.out.println( "Listening on port 8119...");
-        }catch(Exception e){
+    public static void main(String[] args) throws Exception {
+        HttpConnection httpConnection = new HttpConnection();
+        try {
+            httpConnection.startServer();
+        }catch (IOException e){
             e.printStackTrace();
         }
     }
